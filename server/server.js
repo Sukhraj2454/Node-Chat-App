ecc3cd8071a40dcc19d3541f72e625ec4cb7d2a2
Sukhraj Singh <sukhraj2454@gmail.com>
2020-10-03 19:36:45 +0530

    greet new user and alert new users

--- server/server.js
+++ server/server.js
@@ -12,18 +12,34 @@ const publicpath = path.join(__dirname, '/../public');
 app.use(express.static(publicpath));
 
 io.on('connection', (socket)=>{
-  console.log('New connection found.');
+  console.log('New user Connected');
 
-  socket.emit('newMessage', {
-    createdAt:123,
-    from:'John',
-    text:'Sorry! ,we are fixed priced shop.'
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the Chat'
   });
+
+socket.broadcast.emit('newMessage',{
+  from:'Admin',
+  text:'New user Joined',
+  createdAt:new Date().getTime()
+});
+
   socket.on('createMessage', (message)=>{
     console.log('Message:', message);
 
-  });
+    io.emit('newMessage',{
+       from:message.from,
+       text:message.text,
+       createdAt:new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    // });
 
+});
   socket.on('disconnect', ()=>{
     console.log('User Disconnected');
   });
